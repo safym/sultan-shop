da44fce797006bf7c845e0048db9f628b0609323
safym <osafym@gmail.com>
2023-03-29 20:46:15 +0300

    add bread crumps render

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,15 +1,52 @@
+import { NavLink } from "react-router-dom";
+import useBreadcrumbs from "use-react-router-breadcrumbs";
+import { useAppSelector } from "../app/hooks";
+import ProductDetails from "./ProductDetails/ProductDetails";
+
 const Breadcrumbs: React.FC = () => {
+  const productsItems = useAppSelector((state) => state.products.productsItems);
+  const breadcrumbs = useBreadcrumbs();
+
+  const getPathTitle = (path: string) => {
+    console.log(path)
+    switch (true) {
+      case path === '/':
+        return "Главная"
+      case path === '/cart':
+        return "Коризна"
+      case path === '/products':
+        return "Каталог"
+      case path.includes('/products/'):
+        const productId = path.replace('/products/', '');
+        const product = productsItems[productId];
+        return `${product.brand} ${product.name}`
+    }
+  }
+  
   return (
     <header className="content__page-header page-header">
       <div className="page-header__container _container">
-          <div className="page-header__path path">
-            <a className="path__step">Главная</a>
-            <span className="path__divider divider divider_size_long"></span>
-            <a className="path__step path__step_state_current">Косметика и гигиена</a>
-          </div>
+        <div className="page-header__path path">
+
+          {breadcrumbs.map(({ breadcrumb, match }, index) => {
+            console.log()
+
+            if (breadcrumb) {
+              console.log(match.pathname)
+
+              return (
+                <span className="path__step" key={match.pathname}>
+                  <NavLink to={match.pathname || ""}>{getPathTitle(match.pathname)}</NavLink>
+                  {index < breadcrumbs.length - 1 && <span className="path__divider divider divider_size_long"></span>}
+                </span>)
+            }
+          })}
+        </div>
+
       </div>
     </header>
   );
 }
+
 
 export default Breadcrumbs;
