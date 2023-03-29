290ec7f498be134aa99fa0b458ff3add16942e6d
safym <osafym@gmail.com>
2023-03-29 20:44:07 +0300

    add scrolltop with routing

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer/Footer";
 import Breadcrumbs from "./Breadcrumbs";
+import ScrollToTop from "./ScrollTop";
 
 const Layout: React.FC = () => {
   return (
     <div className="main-wrapper">
+      <ScrollToTop />
       <Header />
       <div className="content">
         <Breadcrumbs />
