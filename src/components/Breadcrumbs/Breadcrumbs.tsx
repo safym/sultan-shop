6d5bd6c8429fb6be7bdb6e8b15a3934c82fa2efa
safym <osafym@gmail.com>
2023-03-31 20:52:50 +0300

    fix bread crumbs style

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,6 +1,8 @@
 import { NavLink } from "react-router-dom";
 import useBreadcrumbs from "use-react-router-breadcrumbs";
-import { useAppSelector } from "../app/hooks";
+import { useAppSelector } from "../../app/hooks"
+
+import style from "./Breadcrumbs.module.scss"
 
 const Breadcrumbs: React.FC = () => {
   const productsItems = useAppSelector((state) => state.products.productsItems);
@@ -29,20 +31,15 @@ const Breadcrumbs: React.FC = () => {
   }
 
   return (
-    <header className="content__page-header page-header">
-      <div className="page-header__container _container">
-        <div className="page-header__path path">
 
-          {breadcrumbs.map(({ breadcrumb, match }, index) => (
-            <span className="path__step" key={match.pathname}>
-              <NavLink to={match.pathname || ""}>{getPathTitle(match.pathname)}</NavLink>
-              {index < breadcrumbs.length - 1 && <span className="path__divider divider divider_size_long"></span>}
-            </span>
-          ))}
-        </div>
-
+      <div className={style.path}>
+        {breadcrumbs.map(({ breadcrumb, match }, index) => (
+          <span className={style.step} key={match.pathname}>
+            <NavLink to={match.pathname || ""}>{getPathTitle(match.pathname)}</NavLink>
+          </span>
+        ))}
       </div>
-    </header>
+    // </div>
   );
 }
 
