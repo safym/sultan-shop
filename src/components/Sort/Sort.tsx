8f9e68e3440d08a963531d8f49567d6127a8e1a0
safym <osafym@gmail.com>
2023-04-01 01:30:21 +0300

    fix sort component style

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -1,23 +1,14 @@
-import { useEffect, useState } from "react";
-import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import { sortList, sortItem, setSort } from "../../app/slices/sortSlice";
+import { useEffect, useState } from "react"
+import { useAppDispatch, useAppSelector } from "../../app/hooks"
+import { sortList, sortItem, setSort } from "../../app/slices/sortSlice"
 
 import style from "./Sort.module.scss"
 
 const Sort: React.FC = () => {
-  let [dropdownOpen, setDropdownOpen] = useState(false);
+  let [dropdownOpen, setDropdownOpen] = useState(false)
 
-  const sort = useAppSelector((state) => state.sort);
-  const dispatch = useAppDispatch();
-
-  const sortOnClick = () => {
-    setDropdownOpen(!dropdownOpen);
-  }
-
-  const optionOnClick = (item: sortItem) => {
-    dispatch(setSort({ item }))
-    setDropdownOpen(!dropdownOpen);
-  }
+  const sort = useAppSelector((state) => state.sort)
+  const dispatch = useAppDispatch()
 
   useEffect(() => {
     const onClickOutSide = (event: MouseEvent) => {
@@ -29,33 +20,35 @@ const Sort: React.FC = () => {
     document.body.addEventListener('click', onClickOutSide);
 
     return () => document.body.removeEventListener('click', onClickOutSide);
-  }, []);
+  }, [])
+
+  const sortOnClick = () => {
+    setDropdownOpen(!dropdownOpen)
+  }
+
+  const optionOnClick = (item: sortItem) => {
+    dispatch(setSort({ item }))
+    setDropdownOpen(!dropdownOpen)
+  }
 
   return (
     <div className={style.sort} id="product-sort">
-      <span className={style.sort__title}>Сортировка:</span>
-      <a className={style.sort__link} href="#">
-
+      <span className={style.title}>Сортировка:</span>
+      <a className={style.link}>
         <span onClick={() => sortOnClick()}>{sort.title}</span>
-
-        {/* <span className="show-hide-link__direction-arrow">
-          <svg width="7" height="6" viewBox="0 0 7 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M3.5 6L0.468911 0.750001L6.53109 0.75L3.5 6Z" fill="#3F4E65" />
-          </svg>
-        </span> */}
       </a>
       {dropdownOpen && (
         <div className={style.dropdown}>
           {sortList.map((item) => (
-            <span className={style.dropdown__option} onClick={() => optionOnClick(item)} key={item.title}>
+            <span className={style.option} onClick={() => optionOnClick(item)} key={item.title}>
               {item.title}
             </span>
           ))}
         </div>
       )}
     </div>
-  );
+  )
 }
 
-export default Sort;
+export default Sort
 
