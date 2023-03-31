b801352b44ece0a9eadd8fe88ad9916633a0d9f7
safym <osafym@gmail.com>
2023-03-31 20:53:16 +0300

    fix layout style

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer/Footer";
-import Breadcrumbs from "./Breadcrumbs";
+import Breadcrumbs from "./Breadcrumbs/Breadcrumbs";
 import ScrollToTop from "./ScrollTop";
+
+import mainStyle from "../scss/_container.module.scss"
 
 const Layout: React.FC = () => {
   return (
     <div className="main-wrapper">
       <ScrollToTop />
       <Header />
-      <div className="content">
+      <div className={mainStyle.container}>
         <Breadcrumbs />
         <Outlet />
       </div>
